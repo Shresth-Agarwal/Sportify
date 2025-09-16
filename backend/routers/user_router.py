b8229a9971db007e9660bbd1e8560c76fe9fdff9
Risspecct <rishikgarg.dev@gmail.com>
2025-09-16 20:33:31 +0530

    corrected auth bearer definition

--- backend/routers/user_router.py
+++ backend/routers/user_router.py
@@ -1,20 +1,21 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from backend.database.db_config import get_db
 from backend.schemas import UserOut
 from backend.services.userService import get_authed_user
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+bearer = HTTPBearer()
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.get("/me", response_model=UserOut)
 def read_current_user(
-    token: str = Depends(oauth2_scheme),
+    credential: HTTPAuthorizationCredentials = Depends(bearer),
     db: Session = Depends(get_db)
 ):
+    token = credential.credentials
     user = get_authed_user(db, token)
     if not user:
         raise HTTPException(status_code=401, detail="Invalid authentication")
--- backend/services/security_service.py
+++ backend/services/security_service.py
@@ -1,16 +1,28 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from backend.token import verify_access_token
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+from backend.database.db_config import get_db
+from backend.models import User
+from backend.token import SECRET_KEY, ALGORITHM
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+security = HTTPBearer()
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    payload = verify_access_token(token)
-    if payload is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return payload.get("sub")  # email stored in "sub"
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: Session = Depends(get_db)
+):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])  # pyright: ignore[reportArgumentType]
+        user_id: str = payload.get("sub")  # pyright: ignore[reportAssignmentType]
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Token missing subject")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+
+    return db.query(User).filter(User.id == int(user_id)).first()
